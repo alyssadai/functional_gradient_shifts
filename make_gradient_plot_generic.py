7a559fa08f906d40b423dc4116a57a8c197248e8
Alyssa Dai <alyssa.dai@mail.mcgill.ca>
2022-11-15 00:35:22 -0500

    update comments in plotting script

--- make_gradient_plot_generic.py
+++ make_gradient_plot_generic.py
@@ -1,5 +1,5 @@
-# Desc: Removes NA values internally.
-# Usage: python make_gradient_plot.py maturational_gradients/merged_comp-labels_gradient-values/merged_LR_comp-labels_child-gradient2-INV-values_zscore_nomidline.txt figures/child-gradient2-INV_zscore_k8_violinplot.png 2 palette_hex.txt
+# Description: Plots component-specific gradient values from a given gradient map as violin+box plots. Removes NA values internally.
+# Example usage: python make_gradient_violin_plot.py gradient_maps_zscore/merged_LR_comp-labels_adult-gradient1-values_zscore_nomidline.txt figures/adult-gradient1_zscore_k8_violinplot.png 1 palette_hex.txt
 # Note: input file should not include midline vertices
 
 import numpy as np
@@ -11,14 +11,13 @@ plt.switch_backend('Agg')
 import seaborn as sns
 import sys
 
-in_file = sys.argv[1]
-out_file = sys.argv[2]
-gradient_num = sys.argv[3] # for x-axis of plot
-palette_path = sys.argv[4]
-# mypalette = np.loadtxt(palette_path, dtype=str, comments=None)
-mypalette = np.genfromtxt(palette_path, delimiter=',')
+in_file = sys.argv[1]       # gradient map .txt file containing 2 columns: 1) vertex component labels 2) vertex gradient values
+out_file = sys.argv[2]      # file path for output plot
+gradient_num = sys.argv[3]  # gradient number (e.g. 1 for gradient 1, 2 for gradient 2)
+palette_path = sys.argv[4]  # path to .txt files with k hex codes to use for k component violin plots
 
-gradient_df = pd.read_csv(in_file, delimiter = " ", names = ["Component", f"Gradient {gradient_num}"]) # MODIFY
+mypalette = np.genfromtxt(palette_path, delimiter=',')
+gradient_df = pd.read_csv(in_file, delimiter = " ", names = ["Component", f"Gradient {gradient_num}"])
 gradient_df.dropna(inplace = True)
 
 sns.set_style("ticks")
@@ -31,7 +30,7 @@ plt.setp(ax.collections, alpha=.9)
 
 mygray = "#3D3D3D"
 sns.boxplot(ax = ax, x=f"Gradient {gradient_num}", y="Component", orient="h", data=gradient_df,
-            linewidth=0, showcaps=False, showmeans=True, showfliers=False, width=0.07, #color="black",
+            linewidth=0, showcaps=False, showmeans=True, showfliers=False, width=0.07,
             boxprops=dict(facecolor=mygray, zorder=10),
             medianprops=dict(color="w", linewidth=3, solid_capstyle="butt", zorder=20),
             meanprops=dict(marker="s", markeredgecolor="none", markerfacecolor="silver", markersize=6, zorder=20),
